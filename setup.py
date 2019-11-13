53825257e2012c12bfba90019078a20113638dc1
Gyeongsu Yim <point1304@gmail.com>
2019-11-13 09:46:37 +0900

    update setup.py (dependency declaration)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='insta_hashtag_crawler',
-    version='0.0.1',
+    version='0.0.2',
     author='Gyeongsu Yim',
     author_email='point1304@gmail.com',
     description='A gevent-based simple instagram hashtag crawler',
@@ -19,6 +19,10 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'gevent>=1.4.0',
+        'requests>=2.22.0',
+    ],
     entry_points={
         'console_scripts': [
             'insta-crawl = insta_hashtag_crawler.command:crawl'
