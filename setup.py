ee056f91d14e19404335fcc49360942acc2e15e8
K.S. Yim <point1304@gmail.com>
2020-07-06 22:57:29 +0900

    git repo change/update in setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='insta_hashtag_crawler',
-    version='0.0.3',
-    author='Gyeongsu Yim',
+    name='insta-hashtag-crawler',
+    version='0.0.4',
+    author='K.S. Yim',
     author_email='point1304@gmail.com',
     description='A gevent-based simple instagram hashtag crawler',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/pypa/smpleproject',
+    url='https://github.com/point1304/insta-hashtag-crawler',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
